5db9e567565b7e45a68e3b75e3619f115b1bdf35
valdopery <valdopierre032@gmail.com>
2019-03-28 01:01:35 +0530

    Files of the Currency converter

---
+++ app/src/main/java/com/valdo/currencyconverter/MainActivity.java
@@ -0,0 +1,90 @@
+package com.valdo.currencyconverter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int ra;
+
+
+    public void convertF(View view){
+
+        EditText moneyE=(EditText)findViewById(R.id.moneyEdit);
+
+        Log.i("Number genrated",""+ra);
+        int n=Integer.parseInt(moneyE.getText().toString());
+         if(n>ra)
+             Toast.makeText(getApplicationContext(),"The number you've entered is greater",Toast.LENGTH_LONG).show();
+         else
+             if(n<ra)
+                 Toast.makeText(getApplicationContext(),"The number you've entered is smaller",Toast.LENGTH_LONG).show();
+             else {
+                 Toast.makeText(getApplicationContext(), "Congrats!! you won!", Toast.LENGTH_LONG).show();
+
+             }
+
+    }
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Random randomGen= new Random();
+        ra=randomGen.nextInt(100);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
